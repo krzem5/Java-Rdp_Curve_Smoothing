a42143d88d5cbe4360134814a7a7f04aa5bb9a5b
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:23:31 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/rdp_curve_smoothing/Main.java&&java com/krzem/rdp_curve_smoothing/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/rdp_curve_smoothing/Main.java&&jar cvmf ../manifest.mf ../build/rdp_curve_smoothing.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="rdp_curve_smoothing.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/rdp_curve_smoothing.jar
+:end
